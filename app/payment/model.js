b84e984f984e090c2640d8beebf59883eb4fb2b2
amarNrddn <amarnrddn02@gmail.com>
2024-02-27 15:41:22 +0700

    feat: make model payment

---
+++ app/payment/model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const { model, Schema } = mongoose
+
+const paymentSchema = Schema({
+   type: {
+      type: String,
+      require: [true, 'Type Pembayaran harus di isi!']
+   },
+   status: {
+      type: String,
+      enum: ['Active', 'Non-Active'],
+      default: 'Active'
+   },
+   banks: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Bank'
+   }
+})
+
+module.exports = model('Payment', paymentSchema)
